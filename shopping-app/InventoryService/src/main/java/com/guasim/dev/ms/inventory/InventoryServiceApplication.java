646c0e27d6bc9ef9778f273aae3a250d1ded9886
Guasim KH <guasim.dev@gmail.com>
2023-03-24 16:44:17 +0100

    New DiscoveryServer

--- shopping-app/InventoryService/src/main/java/com/guasim/dev/ms/inventory/InventoryServiceApplication.java
+++ shopping-app/InventoryService/src/main/java/com/guasim/dev/ms/inventory/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.guasim.dev.ms.inventory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.guasim.dev.ms.inventory.model.Inventory;
 import com.guasim.dev.ms.inventory.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
